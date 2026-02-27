8c308680ab5ed70832889172e03553a829805549
William Jackson <william@jaackson.com>
2026-02-27 11:46:06 +1000

    Remove tilt option from BadgeComponent, use class instead

--- app/components/ui/badge_component.rb
+++ app/components/ui/badge_component.rb
@@ -12,17 +12,11 @@ module Ui
       sm: "text-xs px-3 py-1"
     }.freeze
 
-    TILT_CLASSES = {
-      left: "-rotate-2",
-      right: "rotate-1"
-    }.freeze
-
-    def initialize(text:, variant: :primary, size: :base, color: nil, tilt: nil, **options)
+    def initialize(text:, variant: :primary, size: :base, color: nil, **options)
       @text = text
       @variant = variant.to_sym
       @size = size.to_sym
       @color = color
-      @tilt = tilt&.to_sym
       @extra_class = options.delete(:class)
       @options = options
     end
@@ -39,7 +33,6 @@ module Ui
         BASE_CLASSES,
         @color ? nil : VARIANT_CLASSES[@variant],
         SIZE_CLASSES[@size],
-        @tilt ? TILT_CLASSES[@tilt] : nil,
         @extra_class
       ].compact.join(" ")
     end
