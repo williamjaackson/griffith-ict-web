0dce1051ad9e13f5d4c4fd0579cafa1a9330aa87
William Jackson <william@jaackson.com>
2026-02-27 18:41:19 +1000

    Feature | Add socials config (#18)

---
+++ config/initializers/socials.rb
@@ -0,0 +1,7 @@
+Rails.application.config.socials = {
+  discord: "https://discord.gg/ESvqmEF4vA",
+  github: "https://github.com/williamjaackson/griffith-ict-web/",
+  linkedin: "https://www.linkedin.com/company/griffithtechorg/",
+  instagram: "http://instagram.com/griffithictclub",
+  email: "griffithictclub@gmail.com"
+}.freeze
