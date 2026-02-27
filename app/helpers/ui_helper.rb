7336029fc790e2ae7c398dbd874576a15677da15
William Jackson <william@jaackson.com>
2026-02-27 11:22:00 +1000

    Add ui_heading helper to UiHelper

--- app/helpers/ui_helper.rb
+++ app/helpers/ui_helper.rb
@@ -2,4 +2,8 @@ module UiHelper
   def ui_button(label, href:, variant:, size: :base, **options)
     render Ui::ButtonComponent.new(label: label, href: href, variant: variant, size: size, **options)
   end
+
+  def ui_heading(text, level: 1, **options)
+    render Ui::HeadingComponent.new(text: text, level: level, **options)
+  end
 end
