e5c2a198da8275ab292490a131286e9808d29df9
William Jackson <william@jaackson.com>
2026-02-27 18:37:31 +1000

    Add email to socials config and use it in sponsorship pages

--- config/initializers/socials.rb
+++ config/initializers/socials.rb
@@ -2,5 +2,6 @@ Rails.application.config.socials = {
   discord: "https://discord.gg/ESvqmEF4vA",
   github: "https://github.com/williamjaackson/griffith-ict-web/",
   linkedin: "https://www.linkedin.com/company/griffithtechorg/",
-  instagram: "http://instagram.com/griffithictclub"
+  instagram: "http://instagram.com/griffithictclub",
+  email: "griffithictclub@gmail.com"
 }.freeze
