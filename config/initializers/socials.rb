c08354b3687d633575a0a0dc61572834d322987b
William Jackson <william@jaackson.com>
2026-03-01 18:27:50 +1000

    Move socials config from Ruby initializer to YAML

--- config/initializers/socials.rb
+++ config/initializers/socials.rb
@@ -1,7 +1,1 @@
-Rails.application.config.socials = {
-  discord: "https://discord.gg/ESvqmEF4vA",
-  github: "https://github.com/williamjaackson/griffith-ict-web/",
-  linkedin: "https://www.linkedin.com/company/griffithtechorg/",
-  instagram: "http://instagram.com/griffithictclub",
-  email: "griffithictclub@gmail.com"
-}.freeze
+Rails.application.config.socials = YAML.load_file(Rails.root.join("config/socials.yml")).symbolize_keys.freeze
