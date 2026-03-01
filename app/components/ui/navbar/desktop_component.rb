c47542b7b7c8fe6e7f324cbf3203db963da5de62
William Jackson <william@jaackson.com>
2026-03-01 14:07:04 +1000

    Extract desktop and mobile navbar into sub-components

---
+++ app/components/ui/navbar/desktop_component.rb
@@ -0,0 +1,18 @@
+module Ui
+  module Navbar
+    class DesktopComponent < ViewComponent::Base
+      def initialize(links:, cta:)
+        @links = links
+        @cta = cta
+      end
+
+      private
+
+      attr_reader :links, :cta
+
+      def underline_span
+        tag.span(class: "absolute -bottom-1 left-1/2 w-0 h-0.5 bg-brand-red transition-all duration-300 group-hover:w-full group-hover:left-0")
+      end
+    end
+  end
+end
---
+++ app/components/ui/navbar/mobile_component.rb
@@ -0,0 +1,14 @@
+module Ui
+  module Navbar
+    class MobileComponent < ViewComponent::Base
+      def initialize(links:, cta:)
+        @links = links
+        @cta = cta
+      end
+
+      private
+
+      attr_reader :links, :cta
+    end
+  end
+end
