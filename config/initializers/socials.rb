fc4c4b1c543d71c8e463f6aa07bd79674d1c5dc3
William Jackson <william@jaackson.com>
2026-03-01 18:47:24 +1000

    Move CampusGroups URLs from component constant to socials.yml

--- config/initializers/socials.rb
+++ config/initializers/socials.rb
@@ -1,1 +1,1 @@
-Rails.application.config.socials = YAML.load_file(Rails.root.join("config/socials.yml")).symbolize_keys.freeze
+Rails.application.config.socials = YAML.load_file(Rails.root.join("config/socials.yml")).deep_symbolize_keys.freeze
