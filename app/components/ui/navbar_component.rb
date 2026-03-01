8bec56afea9c25e350cb608e9ca7f42f909ea76d
William Jackson <william@jaackson.com>
2026-03-01 13:45:10 +1000

    Add chevron_svg and underline_span helpers to navbar component

--- app/components/ui/navbar_component.rb
+++ app/components/ui/navbar_component.rb
@@ -17,13 +17,15 @@ module Ui
     def links = LINKS
     def cta = CTA
 
-    def chevron_svg(size: "w-3.5 h-3.5", **attrs)
+    def chevron_svg(css_class:)
       tag.svg(
         tag.path(d: "M19 9l-7 7-7-7", stroke_linecap: "round", stroke_linejoin: "round"),
-        class: "#{size} transition-transform duration-200",
-        fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", stroke_width: "2.5",
-        **attrs
+        class: css_class, fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", stroke_width: "2.5"
       )
+    end
+
+    def underline_span
+      tag.span(class: "absolute -bottom-1 left-1/2 w-0 h-0.5 bg-brand-red transition-all duration-300 group-hover:w-full group-hover:left-0")
     end
   end
 end
