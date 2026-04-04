1faa2fd487daa459b6063b56785b1d3e2a80b04d
William Jackson <william@jaackson.com>
2026-04-04 14:00:22 +1000

    Add role enum to User model (admin, exec)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
 
+  enum :role, { admin: 0, exec: 1 }
+
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
