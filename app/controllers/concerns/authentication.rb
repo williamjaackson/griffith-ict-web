b70008b8a6aac56d4d0847f4cdc84fe1f09ead84
William Jackson <william@jaackson.com>
2026-04-04 18:10:33 +1000

    Expire sessions after 30 days and add secure cookie flag

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -26,7 +26,9 @@ module Authentication
     end
 
     def find_session_by_cookie
-      Session.find_by(id: cookies.signed[:session_id]) if cookies.signed[:session_id]
+      if cookies.signed[:session_id]
+        Session.where("created_at > ?", 30.days.ago).find_by(id: cookies.signed[:session_id])
+      end
     end
 
     def request_authentication
@@ -41,7 +43,7 @@ module Authentication
     def start_new_session_for(user)
       user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session|
         Current.session = session
-        cookies.signed.permanent[:session_id] = { value: session.id, httponly: true, same_site: :lax }
+        cookies.signed[:session_id] = { value: session.id, httponly: true, same_site: :lax, secure: Rails.env.production?, expires: 30.days.from_now }
       end
     end
 
