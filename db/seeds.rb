ca4dcd160da7a34e04c005d5172529be6281c662
William Jackson <william@jaackson.com>
2026-04-04 18:12:21 +1000

    Raise if ADMIN_PASSWORD is missing in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
+password = if Rails.env.production?
+  ENV.fetch("ADMIN_PASSWORD") { raise "ADMIN_PASSWORD must be set in production" }
+else
+  ENV.fetch("ADMIN_PASSWORD", "password")
+end
+
 User.find_or_create_by!(email_address: "admin@griffithict.club") do |user|
-  user.password = ENV.fetch("ADMIN_PASSWORD", "password")
+  user.password = password
   user.role = :admin
 end
