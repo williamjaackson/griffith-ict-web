618e550d15acbced5b83f52b00fb4c5c17f6b077
William Jackson <william@jaackson.com>
2026-07-17 16:49:01 +1000

    Document multi-step UI in Lookbook

--- test/components/previews/site/membership_dialog_component_preview.rb
+++ test/components/previews/site/membership_dialog_component_preview.rb
@@ -1,5 +1,6 @@
 module Site
   class MembershipDialogComponentPreview < ViewComponent::Preview
+    # Membership composes Dialog, Steps, and Button without constructing content in JavaScript.
     def default
       render_with_template
     end
